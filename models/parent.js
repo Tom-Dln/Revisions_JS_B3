17c40d6dcb2e2487d32d0818d39f0a69229f82bd
Tom-Dln <tom.delaunay.perso@gmail.com>
2022-09-28 11:40:56 +0200

    Modifications BDD

--- models/parent.js
+++ models/parent.js
@@ -3,9 +3,9 @@ import fs from "fs";
 const getParent = (parentId) => {
     let BDD = fs.readFileSync("./BDD.json");
     let parents = JSON.parse(BDD).parents;
-    let parent = parents.find(element => element.id == parentId)
+    let parent = parents.find(element => element.id == parentId);
     console.log(parent);
     return parent;
-}
+};
 
 export default getParent;
--- models/student.js
+++ models/student.js
@@ -3,9 +3,9 @@ import fs from "fs";
 const getStudent = (studentId) => {
     let BDD = fs.readFileSync("./BDD.json");
     let students = JSON.parse(BDD).students;
-    let student = students.find(element => element.id == studentId)
+    let student = students.find(element => element.id == studentId);
     console.log(student);
     return student;
-}
+};
 
 export default getStudent;
